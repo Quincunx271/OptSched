65c7f772d8031bf8cdc49099dbfa5d5f4870733e
Justin Bassett <jbassett271@gmail.com>
2020-07-21 17:44:09 -0700

    Fix Transitive Reduction

--- lib/Scheduler/tred_graph_trans.cpp
+++ lib/Scheduler/tred_graph_trans.cpp
@@ -7,9 +7,16 @@
 
 using namespace llvm::opt_sched;
 
+#ifdef IS_DEBUG_TRED
+#define DEBUG_LOG(...) Logger::Info(__VA_ARGS__)
+#else
+#define DEBUG_LOG(...) static_cast<void>(0)
+#endif
+
 template <typename F>
 static void DepthFirstVisit_(llvm::SmallPtrSetImpl<GraphNode *> &seen,
                              GraphNode *node, F f) {
+  DEBUG_LOG("   DepthFirstSearch at %d", node->GetNum());
   seen.insert(node);
   f(node);
 
@@ -24,6 +31,7 @@ static void DepthFirstVisit_(llvm::SmallPtrSetImpl<GraphNode *> &seen,
 template <typename F>
 static void DepthFirstVisit(llvm::SmallPtrSetImpl<GraphNode *> &seen,
                             GraphNode *node, F f) {
+  DEBUG_LOG("  DepthFirstSearch from %d", node->GetNum());
   seen.clear();
   DepthFirstVisit_(seen, node, f);
 }
@@ -48,11 +56,15 @@ FUNC_RESULT TransitiveReductionTrans::ApplyTrans() {
 
     // TODO: evaluate if graph->GetInstByTplgclOrdr(i); would be faster.
     SchedInstruction *u = graph->GetInstByIndx(i);
+    DEBUG_LOG(" Checking from u = %d", u->GetNum());
 
     for (GraphEdge &edge : u->GetSuccessors()) {
       GraphNode *v = edge.to;
 
       ::DepthFirstVisit(depthFirstHasSeen, v, [&](GraphNode *vp) {
+        if (vp == v) {
+          return;
+        }
         GraphEdge *uvp = u->FindScsr(vp);
         if (uvp) {
           edgesToRemove.push_back(uvp);
@@ -64,6 +76,7 @@ FUNC_RESULT TransitiveReductionTrans::ApplyTrans() {
         GraphNode *vp = e->to;
         u->RemoveSuccTo(vp);
         vp->RemovePredFrom(u);
+        DEBUG_LOG("  Deleting GraphEdge* at %p", (void *)e);
         delete e;
       }
     }
