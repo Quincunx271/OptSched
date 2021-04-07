b74e12cbc49ac331069617e27f90dffb49014c6f
Justin Bassett <jbassett271@gmail.com>
2021-04-07 00:49:28 -0700

    More analysis

--- util/gt_analysis/gt_cmp.py
+++ util/gt_analysis/gt_cmp.py
@@ -34,6 +34,38 @@ def _enum_time(blk):
     return blk.single('DagSolvedOptimally')['time'] - blk['Enumerating'][0]['time']
 
 
+def _cost(blk):
+    return blk.single('BestResult')['cost'] + blk.single('CostLowerBound')['cost']
+
+
+def _is_improved(before, after):
+    return _cost(before) > _cost(after)
+
+
+def _is_timed_out(blk):
+    return 'DagTimedOut'
+
+
+def _cost_improvement(before, after):
+    return _cost(before) - _cost(after)
+
+
+def _unary_pred(pred):
+    return lambda a, b: pred(a) and pred(b)
+
+
+def _dual_filter(first, second, pred):
+    joint_p = [(f, s) for f, s in zip(first.all_blocks(), second.all_blocks()) if pred(f, s)]
+
+    first_p = set(f.uniqueid() for f, _ in joint_p)
+    second_p = set(s.uniqueid() for _, s in joint_p)
+    both_p = first_p & second_p
+
+    first = first.keep_blocks_if(lambda blk: blk.uniqueid() in both_p)
+    second = second.keep_blocks_if(lambda blk: blk.uniqueid() in both_p)
+
+    return first, second
+
 class RawCombinedRpIlpGtTime(analyze.Analyzer):
     def run_bench(self, args):
         logs = args[0]
@@ -71,14 +103,16 @@ class GtCmp(analyze.Analyzer):
         num_not_opt_not_imp = block_stats.NumNotOptimalNotImproved()
         total_gt_time = TotalGtTime()
 
-        nogt_opt = set(blk.uniqueid()
-                       for blk in blocks_enumerated_optimally(nogt.all_blocks()))
-        gt_opt = set(blk.uniqueid()
-                     for blk in blocks_enumerated_optimally(gt.all_blocks()))
-        both_opt = nogt_opt & gt_opt
 
-        nogt = nogt.keep_blocks_if(lambda blk: blk.uniqueid() in both_opt)
-        gt = gt.keep_blocks_if(lambda blk: blk.uniqueid() in both_opt)
+        nogt_timeout, gt_timeout = _dual_filter(nogt, gt, _unary_pred(_is_timed_out))
+        nogt_to_imp, gt_to_imp = _dual_filter(nogt_timeout, gt_timeout, _is_improved)
+        nogt_to_worse, gt_to_worse = _dual_filter(nogt_timeout, gt_timeout, lambda a, b: _cost(a) < _cost(b))
+        timeout_imp = sum(_cost_improvement(before, after) for before, after in zip(nogt_to_imp.all_blocks(), gt_to_imp.all_blocks()))
+        timeout_imp_baseline = sum(_cost(before) for before in nogt_to_imp.all_blocks())
+        timeout_worse = sum(_cost_improvement(before, after) for before, after in zip(nogt_to_worse.all_blocks(), gt_to_worse.all_blocks()))
+
+
+        nogt, gt = _dual_filter(nogt, gt, _unary_pred(lambda blk: 'DagSolvedOptimally' in blk or 'HeuristicScheduleOptimal' in blk))
 
         self.stat(
             nogt_sched_time=sched_time.calc(nogt.all_blocks()),
@@ -100,6 +134,12 @@ class GtCmp(analyze.Analyzer):
             gt_num_not_optimal_and_not_improved=num_not_opt_not_imp.calc(
                 gt.all_blocks()),
             total_gt_time=total_gt_time.calc(gt.all_blocks()),
+            num_timed_out=len(list(nogt_timeout.all_blocks())),
+            num_timed_out_improved=len(list(nogt_to_imp.all_blocks())),
+            num_timed_out_worse=len(list(nogt_to_worse.all_blocks())),
+            timeout_improvement=timeout_imp,
+            timeout_imp_baseline=timeout_imp_baseline,
+            timeout_worsen=timeout_worse,
         )
 
 
